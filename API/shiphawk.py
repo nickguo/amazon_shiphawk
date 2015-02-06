5c4eaa1456ed3c04ac727d537db119f2dfebb844
Nick Guo <nick.n.guo@gmail.com>
2015-02-06 11:13:03 +0000

    changed 'server' to 'API' and fixed item not found glitch that was causing the hang

---
+++ API/shiphawk.py
@@ -0,0 +1,54 @@
+import requests
+import json
+
+def ShiphawkPrice(params):
+
+    url = 'https://stage.shiphawk.com/api/v1/rates/full?api_key=b230a747c492a66dd7d47b5f9b9dcf26'
+
+    packed = False 
+    itemData = []
+
+    toZip = -1
+    fromZip = -1
+
+    for x in params:
+        itemId = getItem(x['title'])
+        if itemId == -1:
+            continue
+        print 'ITEMID: ', itemId
+        fromZip = x['from_zip']
+        toZip = x['to_zip']
+        width = x['width']
+        height = x['height']
+        length = x['length']
+        weight = x['weight']
+        value = x['price']
+        itemData.append({'width':float(width), 'length':float(length),'height':float(height), 'weight': float(weight), 'value': value, 'id': itemId, 'packed' : packed}) 
+
+    data = {'rate-filter': 'consumer', 'from_zip' : str(fromZip),
+            'to_zip' : str(toZip), 'items' : itemData}
+    data = json.dumps(data)
+    print data
+    headers = {'Content-Type' : 'application/json'} 
+    req = requests.post(url, data=data,headers=headers)
+    
+    req = req.content
+    json2 = json.loads(req)
+
+    if 'error' in json2:
+        return -1
+
+    return json2[0]['price']
+
+
+def getItem(myStr):
+    url = 'https://stage.shiphawk.com/api/v1/items/search/"' + myStr + '"?api_key=b230a747c492a66dd7d47b5f9b9dcf26'
+    req = requests.get(url)
+    req = req.content
+    json3 = json.loads(req)
+    print '\n\n\n', json3, '\n\n\n'
+    if 'error' not in json3:
+        if 'id' in json3[0]:
+            return json3[0]['id']
+    return -1
+    
